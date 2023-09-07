708c7021d7474799d993f08ffe4ec1c90b2c5214
AmnaHussien <amnahussin88@gmail.com>
2023-09-07 13:25:13 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : C program that print  with put function
+ *
+ * Return : always 0 (success)
+ *
+ *
+ * 
+ */
+
 int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+{puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
