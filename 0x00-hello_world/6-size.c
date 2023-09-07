afc9adea27d6d734834444a40f46024e505f1d2d
AmnaHussien <amnahussin88@gmail.com>
2023-09-07 15:28:52 -0700

    my first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: C programm prints with sizeof function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("Size of an char:%ld\n", sizeof(char));
+printf("Size of an int:%ld\n", sizeof(int));
+printf("Size of an long int:%ld\n", sizeof(long int));
+printf("Size of an long long int:%ld\n", sizeof(long long int));
+printf("Size of an float:%ld\n", sizeof(float));
+}
