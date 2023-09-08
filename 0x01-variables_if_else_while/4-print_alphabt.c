c7dddc4f644c42a2ebb8966f7d3061f021d1901f
AmnaHussien <amnahussin88@gmail.com>
2023-09-08 13:11:42 -0700

    iy

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+if (ch == !e && ch == !q)
+{putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
