d3fbb50998e5e41c04c64aebfe7444b797bad4a4
AmnaHussien <amnahussin88@gmail.com>
2023-09-08 11:47:56 -0700

    u

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ *Return: Always 0 (success)
+ */
+int main(void)
+{int i;
+while (i <= 9)
+{putchar(i + 48)
+i++;
+}
+return (0);
+
+
+}
