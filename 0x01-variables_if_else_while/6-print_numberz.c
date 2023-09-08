28bd77c7b304bd0224e256b636021ba600c628ba
AmnaHussien <amnahussin88@gmail.com>
2023-09-08 14:06:26 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ *Return: Always 0 (success)
+ */
+int main(void)
+{int i;
+for (i = 0 ; i <= 9 ; i++)
+{putchar(i)
+}
+putchar('\n');
+return (0);
+}
