cef4c23ccbf27bb204f5a0a6045a4756c8e71023
AmnaHussien <amnahussin88@gmail.com>
2023-09-08 09:45:23 -0700

    kj

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:C program
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 5)
+{printf("", )
+ }
+else  (n == 0)
+{printf("", )
+}
+ if (n n < 6 && n =! 0)
+{printf("%i Last digit of", n , "is",)
+}
+
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char c = 'a';
+while (c < = 'z')
+{putchar(c);
+c++;
+}
+return (0);
+}
