857d1f93ba5fbd0e811d26ee1d7ba3801a0d4d95
AmnaHussien <amnahussin88@gmail.com>
2023-09-08 07:35:57 -0700

    o

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%i is positive\n", n);
+printf(n, "%i is positive\n");
 }
 else if (n == 0)
 {
-printf("%i is zero\n", n);
+printf(n, "%i is zero\n");
 }
 else
 {
-printf("%i is negative\n", n);
+printf(n, "%i is negative\n");
 }
 return (0);
 }
