c36028af5fad67c3db5f12f316dd7ff621e5521b
AmnaHussien <amnahussin88@gmail.com>
2023-09-09 04:42:09 -0700

    mh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ *Return: Always 0 (success)
+ */
+int main(void)
+{int digit = 0, ch = 0;
+while (digit <= 100 || ch <= 100)
+{
+if (digit % 10 != ch % 10)
+putchar(digit + 48);
+putchar(ch + 48);
+digit++;
+ch++;
+putchar(',');
+putchar('');
+}
+putchar('\n');
+return (0);
+}
