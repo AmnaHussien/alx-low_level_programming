875a1722c55e689fea592d245883962cfa86e352
AmnaHussien <amnahussin88@gmail.com>
2023-09-09 12:22:47 -0700

    jo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,26 +8,26 @@
  */
 int main(void)
 {
-int digit = 0, ch;
-while (digit <= 9)
-{
-ch = 0;
-while (ch <= 9)
-{
-if (digit != ch && digit < ch)
-{
-putchar(digit + 48);
-putchar(ch + 48);
-if (digit != 8 && ch != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-ch++;
-}
-digit++;
-}
+  int digit = 0, ch;
+ while (digit <= 9)
+  {
+     ch = 0;
+    while (ch <= 9)
+       {
+            if (digit != ch && digit < ch)
+             { 
+                putchar(digit + 48);
+                putchar(ch + 48);
+                if (digit != 8 && ch !
+                 {  
+                   putchar(',');
+                   putchar(' ');
+                 }
+             }
+            ch++;
+       }
+     digit++;
+  }
 putchar('\n');
 return (0);
 }
