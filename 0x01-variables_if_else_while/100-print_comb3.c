cb1d9e62410c0d373e62e7318463c5484b60aa9e
AmnaHussien <amnahussin88@gmail.com>
2023-09-09 05:57:29 -0700

    k

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,16 @@ int main(void)
 {int digit = 0, ch = 0;
 while (digit <= 100 && ch <= 100)
 {
+putchar(digit);
+while (ch <= 100)
+{
 if (digit % 10 != ch % 10)
-putchar(digit + 48);
 putchar(ch + 48);
 digit++;
 ch++;
 putchar(',');
 putchar(' ');
-}
+}}
 putchar('\n');
 return (0);
 }
