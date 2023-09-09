4ac727b08872a0672303a3cc3bdaeac5ea4086b7
AmnaHussien <amnahussin88@gmail.com>
2023-09-09 15:06:24 -0700

    kj

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,30 +1,38 @@
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * Description: C programm
- *Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
-{int digit = 0, ch;
-while (digit <= 99)
 {
-digit = ch;
-while (ch <= 99)
+int digit = 0, ch, ch2, ch3;
+while (digit <= 9)
 {
-if (digit != ch)
+ch = 0;
+while (ch <= 9)
 {
-putchar((digit / 10) + 48);
-putchar ((digit % 10) + 48);
+ch2 = 0;
+while (ch2 <= 9)
+{ch3 = 0;
+if (digit != ch &&
+digit < ch && ch != ch2 &&
+ch < ch2 && ch2 != ch3 && ch2 < ch3)
+{putchar(digit + 48);
+putchar(ch + 48);
 putchar(' ');
-putchar((ch / 10) + 48);
-putchar((ch % 10) + 48);
-if (digit != 98 || ch != 99)
-{
-putchar(',');
+putchar(ch2 + 48);
+putchar(ch3 + 48);
+if (digit + ch + ch2 + ch3 != 116)
+{putchar(',');
 putchar(' ');
 }
+}
+ch3++;
+}
+ch2++;
 }
 ch++;
 }
@@ -33,3 +41,5 @@ digit++;
 putchar('\n');
 return (0);
 }
+
+
