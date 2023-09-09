3e8cfe0f443bb17a2ad7cd2a1fa54e6fb85f435b
AmnaHussien <amnahussin88@gmail.com>
2023-09-09 13:46:20 -0700

    kjh

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int digit = 0, ch, ch2;
+while (digit <= 9)
+{
+ch = 0;
+while (ch <= 9)
+{
+ch2 = 0;
+while (ch2 <= 9)
+{if (digit != ch != ch2 && digit < ch < ch2)
+{putchar(digit + 48);
+putchar(ch + 48);
+putchar(ch2 + 48);
+if (digit + ch + ch2 != 24)
+{putchar(',');
+putchar(' ');
+}
+}
+ch2++;
+}
+ch++;
+}
+digit++;
+}
+putchar('\n');
+return (0);
+}
+
