01ccb2915aef747e3c158ba49be2a61eccfc8201
AmnaHussien <amnahussin88@gmail.com>
2023-09-10 12:20:47 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,11 @@ while (ch <= 9)
 ch2 = 0;
 while (ch2 <= 9)
 {ch3 = 0;
-putchar(digit + 48);
+while (ch3 <= 9)
+{
+if (digit != ch && digit < ch &&
+ch != ch2 && ch < ch2 && ch2 != ch3 && ch2 < ch3)
+{putchar(digit + 48);
 putchar(ch + 48);
 putchar(' ');
 putchar(ch2 + 48);
@@ -25,6 +29,9 @@ putchar(ch3 + 48);
 if (digit + ch + ch2 + ch3 != 116)
 {putchar(',');
 putchar(' ');
+}
+}
+ch3++;
 }
 ch2++;
 }
