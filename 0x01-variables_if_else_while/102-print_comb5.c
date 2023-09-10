682ff119f7b8a0c8e987d33b8177b27572859fcc
AmnaHussien <amnahussin88@gmail.com>
2023-09-10 14:05:19 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,31 +8,22 @@
  */
 int main(void)
 {
-int digit = 0, ch, ch2, ch3;
-while (digit <= 9)
+int digit = 0, ch;
+while (digit <= 99)
 {
 ch = 0;
-while (ch <= 9)
+while (ch <= 99)
 {
-ch2 = 0;
-while (ch2 <= 9)
-{ch3 = 0;
-while (ch3 <= 9)
-{
-if (digit <= ch && ch2 <= ch3)
-{putchar(digit + 48);
-putchar(ch + 48);
+if (digit != ch)
+{putchar((digit / 10) + 48);
+putchar((digit % 10) + 48);
 putchar(' ');
-putchar(ch2 + 48);
-putchar(ch3 + 48);
-if (digit + ch + ch2 + ch3 != 116)
+putchar((ch / 10) + 48);
+putchar((ch % 10) + 48);
+if (digit != 98 && ch != 99)
 {putchar(',');
 putchar(' ');
 }
-}
-ch3++;
-}
-ch2++;
 }
 ch++;
 }
