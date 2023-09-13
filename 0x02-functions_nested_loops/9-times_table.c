3d2870f8b14ac3b2bd31ed3c9e88882d6cc83213
AmnaHussien <amnahussin88@gmail.com>
2023-09-13 13:40:24 -0700

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,11 @@ void times_table(void)
 int a, b, c;
 for (a = 0; a <= 9; a++)
 {
+_putchar('0');
 for (b = 0; b <= 9; b++)
 {
-_putchar('0');
+_putchar(',');
+_putchar(' ');
 c = a * b;
 if (c <= 9)
 _putchar(' ');
