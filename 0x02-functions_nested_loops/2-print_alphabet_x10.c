5b08f3f5765ad01f59e7490a468d61bc3629cc5e
AmnaHussien <amnahussin88@gmail.com>
2023-09-13 04:41:08 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_10 - function  print alphabet from a to z 10 time
+ * Description: print alphabet 10 time
+ */
+void  print_alphabet_x10(void)
+{
+int ch, i;
+for (i = 1; i <= 10 ; i++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+}
