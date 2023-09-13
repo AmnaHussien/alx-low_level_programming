eb0d78ecab7b7781233ff65953be64f820ae0f0b
AmnaHussien <amnahussin88@gmail.com>
2023-09-13 15:13:20 -0700

    done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ * Return: Always 0 (success)
+ */
+int main(void)
+{int n, i;
+for (i = 0; i < 1024; i++)
+{
+if (n == 3 * i && n == 5 * i)
+{
+printf("%d\n ", n);
+}
+}
+}
