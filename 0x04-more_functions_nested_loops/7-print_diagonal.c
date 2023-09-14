b27faff8db8c97d65f8d2e3fdd103e2eb3131b60
AmnaHussien <amnahussin88@gmail.com>
2023-09-14 15:31:06 -0700

    kjh

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,13 @@ putchar('\n');
 }
 else
 {
-for (i = 1; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
 for (space = 1; space <= i ; space++)
 {
 putchar(' ');
 }
-_putchar(92);
+putchar(92);
 putchar('\n');
 }
 }
