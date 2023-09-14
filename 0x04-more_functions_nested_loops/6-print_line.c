1eb3e7318dca427aae44822e3e48bd1ca58c9a37
AmnaHussien <amnahussin88@gmail.com>
2023-09-14 13:52:19 -0700

    ;ljk

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,17 @@
  */
 void print_line(int n)
 {
+int i;
 if (n <= 0)
 {
-_putchar('\n');
+putchar('\n');
 }
 else
 {
-for (int i = 1; i <= n; i++)
+for (i = 1; i <= n; i++)
 {
-_putchar('_');
-_putchar('\n');
+putchar('_');
+putchar('\n');
 }
 }
 }
