3664ed8ce80a6033a5cb35078c21eda34763a4f0
AmnaHussien <amnahussin88@gmail.com>
2023-09-15 13:20:24 -0700

    done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _sqrt - find square root 
+ * Description: C programm
+ * @n: takes integer type to the function
+ * Return: Always 0 (success)
+ */
+void largest_prime_factor(long int n)
+{
+while (n % 2 == 0)
+{
+n = n / 2;
+}
+for ()
+
+
+
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - function that prints an integer
+ * Description: C programm
+ * @n:the integer to be printed
+ * Return: Always 0 (success)
+ */
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+putchar('_');
+num = -num
+}
+if ((num / 10) > 0)
+{
+print_number(num / 10);
+}
+putchar((num % 10) + 48);
+}
