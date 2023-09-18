9d698b0b565c0bbde1cbf0f9e8a5d5048ad70933
AmnaHussien <amnahussin88@gmail.com>
2023-09-18 07:52:29 -0700

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - function that prints the string in reverse
+ * Description: C programm
+ * @s: character  string input types
+ * Return: Always 0 (success)
+ */
+void print_rev(char *s)
+{char counter;
+for (counter = 0; *s = '\0'; s--)
+{
+_putchar(*s + 0);
+counter++;
+}
+_putchar('\n');
+}
