e3075ea2ef8331dc0019ee35b77f418ce36da2ee
AmnaHussien <amnahussin88@gmail.com>
2023-09-18 10:04:52 -0700

    kh

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,16 @@
  */
 void rev_string(char *s)
 {
-char coun;
-int 1, i;
-for (1 = 0; s[1] != '\0'; ++1)
+char count;
+int l, i;
+for (l = 0; s[l] != '\0'; ++l)
 {
 ;
 }
-for (i = 0; i < 1 / 2; i--)
+for (i = 0; i < l / 2; i++)
 {
 count = s[i];
-s[i] = s[1 - 1 - i];
-s[1 - 1 - i] = count;
+s[i] = s[l - 1 - i];
+s[l - 1 - i] = count;
 }
 }
