02b904ce6edaf383aaac45617bb4b49c439a7051
AmnaHussien <amnahussin88@gmail.com>
2023-09-18 12:40:25 -0700

    done

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: C programm
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int sum;
+char p;
+strand(time(NULL));
+while (sum <= 2645)
+{p = rand() % 128;
+sum += p;
+putchar(p);
+}
+putchar(2772 - sum);
+return (0);
+}
