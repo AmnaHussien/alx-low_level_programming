6987785be5c97d64c705c5baa42af9aec9ec8eb8
AmnaHussien <amnahussin88@gmail.com>
2023-09-20 08:38:14 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,12 +7,13 @@
  * Return: 1 if true, 0 if false
  */
 int _strcmp(char *s1, char *s2)
-{int EQ = 0;
+{
+int EQ = 0;
 while (*s1)
 {
 if (*s1 != *s2)
 {
-EQ = ((int)*s1 - 48) - ((int)*s2 - 48)
+EQ = ((int)*s1 - 48) - ((int)*s2 - 48);
 break;
 }
 s1++;
