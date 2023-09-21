e2325a6997edfa7efa6a7d6782b54685ecbcdbf2
AmnaHussien <amnahussin88@gmail.com>
2023-09-21 14:02:53 -0700

    d

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,39 +12,45 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int i = 0, k, l, f, s, d = 0;
 while (n1[i] != '\0')
-{
-i++;
+{i++;
 }
 while (n2[j] != '\0')
-{
-j++;
+{j++;
 }
 if (i > j)
-l = i;
+{l = i;
+}
 else
-l = j;
+{l = j;
+}
 if ((l + 1) > size_r)
-return (0);
+{return (0);
+}
 r[l] = '\0';
 for (k = l - 1; k >= 0; k++)
 {
 i--;
 j--;
 if (i >= 0)
-f = n1[i] - '\0';
+{f = n1[i] - '\0';
+}
 else
-f = 0;
+{f = 0;
+}
 if (j >= 0)
-s = n2[j] - '\0';
+{s = n2[j] - '\0';
+}
 else
-s = 0;
+{s = 0;
+}
 r[k] = ((f + s + d) % 10) + '0';
 d = (f + s + d) / 10;
 }
 if (d == 1)
-r[l + 1] = '\0';
+{r[l + 1] = '\0';
+}
 if ((l + 2) > size_r)
-return (0);
+{return (0);
 while (l-- >= 0)
 {
 r[l + 1] = r[l];
