a807250062ad84fc50f2e5fd9a44590e68165f8e
AmnaHussien <amnahussin88@gmail.com>
2023-09-25 12:47:33 -0700

    d

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,16 +9,16 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
-for(i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for(j = 0; accept[j] != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-if(accept[j] == s[i])
+if (accept[j] == s[i])
 {
 p = &s[i];
-return(p);
+return (p);
 }
 }
 }
-return(0);
+return (0);
 }
