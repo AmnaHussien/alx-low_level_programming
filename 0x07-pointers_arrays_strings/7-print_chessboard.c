a16a591d718e1a464fde5f82139070e0245b7457
AmnaHussien <amnahussin88@gmail.com>
2023-09-25 13:04:44 -0700

    d

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,14 +4,14 @@
  * print_diagsums -  function that prints the sum of the two diagonals
  * of a square matrix of integers
  * Description: C programm
- * @a: two dimention array of int type 
+ * @a: two dimention array of int type
  * @size: size of array
  * Return: nothing
  */
 void print_diagsums(int *a, int size)
 {
 int i, l = 0, k = 0;
-for(i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 l += a[i];
 k += a[size - i - 1];
