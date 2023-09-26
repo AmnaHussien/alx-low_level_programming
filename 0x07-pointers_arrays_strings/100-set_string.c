f91877be334b13fb0932555a7ce6605b087e40fb
AmnaHussien <amnahussin88@gmail.com>
2023-09-25 19:45:03 -0700

    h

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,8 +2,8 @@
 /**
  * set_string - set the value of pointer to char
  * Description: C programm
- * @s: pointer to pointer
- * @to: pointer to char
+ * @s: input
+ * @to: input
  * Return: always 0 (success)
  */
 void set_string(char **s, char *to)
