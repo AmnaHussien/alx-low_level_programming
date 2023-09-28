21047dd8ada53895e21987d32a4d077e2d6f5b00
AmnaHussien <amnahussin88@gmail.com>
2023-09-28 13:13:47 -0700

    l

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 /**
  * is_prime_number - check if n is prime number
  * @o: int
- * @n: int 
+ * @n: int
  * Return: 0 or 1
  */
 int check_prime_num(int n, int o);
@@ -11,8 +11,8 @@ int is_prime_number(int n)
 {
 return (check_prime_num(n, 2));
 }
-/** 
- * check_prime_num - check  * all number < n 
+/**
+ * check_prime_num - check  * all number < n
  * @n: int
  * @o: int
  * Return: int numer
@@ -29,6 +29,6 @@ return (0);
 }
 else
 {
-return (check_prime_num(n , o + 1))
+return (check_prime_num(n, o + 1));
 }
 }
