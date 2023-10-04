fea5862f9d66ef44e7a1b3f59e03bb57c96918d9
AmnaHussien <amnahussin88@gmail.com>
2023-10-04 13:06:56 -0700

    :

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * *argstostr - description
+ * @ac: int
+ * @av: parameter
+ * Return: string
+ */
+char *argstostr(int ac, char **av)
+{
+int ii = 0, nc = 0, jj = 0, cmp = 0;
+char *ss;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+for (; ii < ac; ii++, nc++)
+{
+nc += _strlen(av[ii]);
+}
+ss = malloc(sizeof(char) * nc + 1);
+if (ss == 0)
+{
+return (NULL);
+}
+for (ii = 0; ii < ac; ii++)
+{
+for (jj = 0; av[ii][jj] != '\0'; jj++, cmp++)
+{
+ss[cmp] = av[ii][jj];
+}
+ss[cmp] = '\n';
+cmp++;
+}
+ss[cmp] = '\0';
+return (ss);
+}
