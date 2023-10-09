175d6ef44be7fcadd6ab010025c4ebcb7eae9091
AmnaHussien <amnahussin88@gmail.com>
2023-10-09 04:00:40 -0700

    d

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,20 +1,6 @@
 #include "main.h"
 /**
- * _strlen - find the length of string
- * @s: string
- * Return: int
- */
-int _strlen(char *s)
-{
-int size = 0;
-for (;s[size] != '\0'; size++)
-{
-;
-}
-return (size);
-}
-/**
- * string_nconcat _ cancat two string
+ * string_nconcat - cancat two string
  * Description: c programm
  * @s1: point to first string
  * @s2: point to second string
@@ -23,16 +9,31 @@ return (size);
  */
 char *string_nconcat(char  *s1, char *s2, unsigned int n)
 {
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-m = malloc(size1 + n + 1)
+unsigned int i, j, size1, size2;
+if (s1 == 0)
+{
+s1 == "";
+}
+if (s2 == 0)
+{
+s2 = "";
+}
+for (size1 = 0; s1[size1] != '\0'; size1++)
+{
+;
+}
+for (size2 = 0; s2[size2] != '\0'; size2++)
+{
+;
+}
+m = malloc(size1 + n + 1);
 if (m == NULL)
 {
 return (NULL);
 }
 for (i = 0; s1[i] != '\0'; i++)
 {
-m[i]= s1[i];
+m[i] = s1[i];
 }
 for (j = 0; j < n; j++)
 {
