07666814b72ddd78dc88133afd336d04270f3b88
AmnaHussien <amnahussin88@gmail.com>
2023-10-10 11:56:54 -0700

    done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: c programm
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
