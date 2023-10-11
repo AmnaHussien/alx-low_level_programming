3fb2dd7fcd659a243973abf8a043a7cf2dda8b19
AmnaHussien <amnahussin88@gmail.com>
2023-10-11 11:26:55 -0700

    d

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,21 +11,16 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (array == 0 || cmp == 0)
-{
-return (0);
-}
-if (!cmp)
-{
-return (-1);
-}
-if (size <= 0)
+if (array == 0 || cmp == 0 || size <= 0)
 {
 return (-1);
 }
 for (i = 0; i < size; i++)
 {
-(*cmp)(array[i]);
+if ((*cmp)(array[i]))
+{
+return (i);
 }
-return (0);
+}
+return (-1);
 }
