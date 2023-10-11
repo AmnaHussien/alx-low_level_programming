bac6c6668ba0e4836eb5e4b14ad4a5e75e523ca6
AmnaHussien <amnahussin88@gmail.com>
2023-10-11 12:22:46 -0700

    l

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,16 +1,24 @@
 #include "calc.h"
 /**
- * (*get_op_func(char *s)) -  perform the operation asked by the user
+ * get_op_func -  perform the operation asked by the user
  * Description: c programm
  * @s: character
  * Return: integer
  */
 int (*get_op_func(char *s))(int, int)
 {
-
-
-
-
-
-
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != '\0' && *(ops[i].op) != *s)
+{
+i++;
+}
+return (ops[i].ff);
 }
