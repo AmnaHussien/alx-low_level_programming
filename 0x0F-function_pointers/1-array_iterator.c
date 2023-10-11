ae55df19ef9de4e378ed829d2353174915e20016
AmnaHussien <amnahussin88@gmail.com>
2023-10-11 09:54:37 -0700

    d

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t int i;
+if (array == 0 || action == 0)
+{
+return;
+}
 for (i = 0; i < size; i++)
 {
 action(array[i]);
