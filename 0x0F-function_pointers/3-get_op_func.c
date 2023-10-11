3a2ddbef2325d7eeefba5b298fd0c7964889d30d
AmnaHussien <amnahussin88@gmail.com>
2023-10-11 12:01:57 -0700

    d

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,16 @@
+#include "calc.h"
+/**
+ * (*get_op_func(char *s)) -  perform the operation asked by the user
+ * Description: c programm
+ * @s: character
+ * Return: integer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
+
+
+
+
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * op_add - add 2 num
+ * Description: c programm
+ * @a: num1
+ * @b: num2
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - sub 2 num
+ * Description: c programm
+ * @a: num1
+ * @b: num2
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul -multiplicate 2 num
+ * Description: c programm
+ * @a: num1
+ * @b: num2
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - division 2 num
+ * Description: c programm
+ * @a: num1
+ * @b: num2
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+return (a \ b);
+}
+/**
+ * op_mod - modulo 2 num
+ * Description: c programm
+ * @a: num1
+ * @b: num2
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
