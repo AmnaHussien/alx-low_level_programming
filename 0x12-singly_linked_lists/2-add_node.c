4ca376046ffd82aaecb69d25373af15574d35c82
AmnaHussien <amnahussin88@gmail.com>
2023-10-20 16:09:25 -0700

    d

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * _stlen - function that return the length of string
+ * Description: c programm
+ * @p: character
+ * Return: the length of string
+ */
+int _stlen(char *p)
+{
+int count = 0;
+if (!p)
+{ return (0); }
+while (*p++)
+{ count++; }
+return (count);
+}
+/**
+ * *add_node - f function that adds new node at the beginning of list_t lis
+ * Description: c programm
+ * @head: pointer
+ * @str: string
+ * Return: the length of string
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *ptr = malloc(sizeof(list_t));
+if (!head || !ptr)
+{ return (0); }
+if (str)
+{
+ptr->str = strdup(str);
+if (!ptr->str)
+{ free(ptr);
+return (0);
+}
+ptr->len = _stlen(ptr->str);
+}
+ptr->next = *head;
+*head = ptr;
+return (ptr);
+}
