5ff6e23f8f5d4db0066eb9ddd4c7a2c50202a2b8
AmnaHussien <amnahussin88@gmail.com>
2023-10-20 15:27:32 -0700

    j

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len -  function that return  number of elements in linked list_t list
+ * Description: c programm
+ * @h: pointer
+ * Return: the numb of elements
+ */
+size_t list_len(const list_t *h)
+{
+size_t counter;
+while (h)
+{
+h = h->next;
+counter++;
+}
+return (counter);
+}
