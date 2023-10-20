1284570e672ad27cdf7e30e128df7f5f930e4aec
AmnaHussien <amnahussin88@gmail.com>
2023-10-20 15:07:20 -0700

    j

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * _stlen - function that return the length of string
+ * Description: c programm
+ * @p: character
+ * Return: the length of string
+ */
+int _stlen(char *p)
+{
+int count = 0;
+if (!p)
+{ return (0); }
+while (*p++)
+{ count++; }
+return (count);
+}
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * Description: c programm
+ * @h: pointer
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{ size_t counter = 0;
+const list_t *ptr = NULL;
+ptr = h;
+while (ptr != NULL)
+{
+printf("[%d] %s\n", _stlen(h->str), h->str ? h->str : "(nil)");
+ptr = ptr->next;
+counter++;
+}
+return (counter);
+}
