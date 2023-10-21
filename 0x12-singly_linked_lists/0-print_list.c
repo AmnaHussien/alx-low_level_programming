75725eadb22db292c6932ae3d52f30230ce171a4
AmnaHussien <amnahussin88@gmail.com>
2023-10-21 08:08:39 -0700

    s

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ return (count);
  * print_list - function that prints all the elements of a list_t list
  * Description: c programm
  * @h: pointer
- * Return: the number of nodes
+ * Return: the size of list
  */
 size_t print_list(const list_t *h)
 { size_t counter = 0;
