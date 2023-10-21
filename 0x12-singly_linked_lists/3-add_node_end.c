6709cea242f969c3eb1deaaf436ef7a6ded48bfb
AmnaHussien <amnahussin88@gmail.com>
2023-10-21 06:50:17 -0700

    f

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *ptr, *temp;
 ptr = *head;
-temp = malloc(sizeof(list_t));
+temp = (list_t *)malloc(sizeof(list_t));
 if (!temp)
 { return (NULL); }
 if (str)
