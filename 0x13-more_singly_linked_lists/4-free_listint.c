4487d14f53851cfc24f42faebfa5344b57c3461a
AmnaHussien <amnahussin88@gmail.com>
2023-10-24 11:31:21 -0700

    done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees list of integer
+ * Description: c programm
+ * @head: pointer
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+listint_t *ptr;
+ptr = head; 
+while (head)
+{
+head = head->next;
+free(ptr);
+}
+}
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees list of integer
+ * Description: c programm
+ * @head: address of pointer to the first node
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *ptr, *temp;
+ptr = *head;
+while (ptr)
+{
+temp = ptr;
+ptr = ptr->next;
+free(temp);
+}
+*head = NULL;
+}
