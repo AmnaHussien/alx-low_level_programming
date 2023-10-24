e0c324ae4f9b7f93ec8e3045181f7081d4566aa7
AmnaHussien <amnahussin88@gmail.com>
2023-10-24 14:55:48 -0700

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *ptr;
 listint_t *temp = malloc(sizeof(listint_t));
-unsigned int j;
+unsigned int j = 0;
 
 if (!head || !*head)
 { return (0); }
