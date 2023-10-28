dbda2b0d1e79740c817a837b6dad78bdeddac137
AmnaHussien <amnahussin88@gmail.com>
2023-10-28 09:45:35 -0700

    d

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,8 @@ void print_binary(unsigned long int n)
 {
 unsigned long int i;
 for (i = 0; i <= n; i++)
-{
-if (n & (1 << i))
-{ printf("1"); }
-else
-{ printf("0"); }
+{ 
+printf("%lu", (n << i) & 1);
 
 }
 }
