7e0b3cfa60cf71a47b7763dc0761515425cbd40a
AmnaHussien <amnahussin88@gmail.com>
2023-10-28 13:34:16 -0700

    d

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,9 +6,8 @@
  * Return: the binary representation of a number
  */
 void print_binary(unsigned long int n)
-{
-unsigned long int i;
-for (i = (1 << 31); i > 0; i--)
+{ int i;
+for (i = sizeof(n) * 8; i > 0; --i)
 {
 (n & i) ? printf("1") : printf("0");
 
